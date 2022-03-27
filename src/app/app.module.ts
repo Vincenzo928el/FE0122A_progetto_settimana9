90b198c667931dd878356d73cb09f2b14bd0858b
Vincenzo928el <vincenzovenezia5@gmail.com>
2022-03-27 22:50:20 +0200

    progetto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule, Route } from '@angular/router';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { CompletatiComponent } from './completati/completati.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Route[] = [
+    {
+      path: "",
+      component: TodosComponent
+    },
+
+    {
+      path: "completati",
+      component: CompletatiComponent
+    }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    CompletatiComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    PickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
